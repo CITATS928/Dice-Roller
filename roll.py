e0d32990dfefd6f11c6fda1139b1463c818ed4ee
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-22 20:44:43 -0400

    Finished

--- roll.py
+++ roll.py
@@ -4,4 +4,24 @@
 # roll 2 - produce two numbers 1-6 and print them
 # roll 3 - produce three numbers and print them.
 #
+import random
 
+
+def roll():
+    return(int(random.randint(1,6)))
+
+def main():
+    print("Your first number is: ",end ="")
+    print(roll())
+    string = ""
+    user_Input = int(input("how many times would you like to run?"))
+    for i in range(1,user_Input+1):
+        if i == user_Input:
+            string +=str(roll())
+            break
+        string +=str(roll())+", "
+    print(string)
+
+if __name__=='__main__':
+    main()
+    
